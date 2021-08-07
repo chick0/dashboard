3e794a6860cb6adb24c37d76fd66417fbab8d56e
chick_0 <chick_0@ch1ck.xyz>
2021-08-07 22:47:25 +0900

    사용자 정보 변경하기

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,12 +1,16 @@
 
 from flask import Blueprint
 from flask import session
+from flask import request
 from flask import redirect
 from flask import url_for
 from flask import render_template
 
+from app import db
+from app.models import User
 from app.check import is_two_factor_enabled
 from app.check import is_login
+from app.mail import send
 from . import login
 from . import register
 from . import delete
@@ -47,8 +51,76 @@ def dashboard():
 
 @bp.post("")
 def user_update():
-    login_user = session.get("user", None)
-    if login_user is None:
+    if not is_login():
         return redirect(url_for("dashboard.login.form"))
+
+    email = request.form.get("email", session['user']['email'])
+    nickname = request.form.get("name", session['user']['nickname'])
+
+    user = User.query.filter_by(
+        idx=session['user']['idx']
+    ).first()
+
+    if nickname != user.nickname:
+        session['user'] = {
+            "idx": user.idx,
+            "email": user.email,
+            "nickname": nickname,
+        }
+
+        user.nickname = nickname
+        db.session.commit()
+
+    if email != user.email:
+        token = send(target_address=email)
+        session['email_update'] = {
+            "email": email,
+            "token": token
+        }
+        return redirect(url_for("dashboard.email_update"))
+
+    return redirect(url_for("dashboard.dashboard"))
+
+
+@bp.get("/email/update")
+def email_update():
+    if not is_login():
+        return redirect(url_for("dashboard.login.form"))
+
+    new_email = session.get("email_update", None)
+    if new_email is None:
+        return redirect(url_for("dashboard.dashboard"))
+
+    return render_template(
+        "dashboard/email_update.html",
+        email=new_email['email']
+    )
+
+
+@bp.post("/email/update")
+def email_update_post():
+    if not is_login():
+        return redirect(url_for("dashboard.login.form"))
+
+    new_email = session.get("email_update", None)
+    if new_email is None:
+        return redirect(url_for("dashboard.dashboard"))
+
+    token = request.form.get("token", None)
+    if token is None or new_email['token'] != token:
+        return redirect(url_for("dashboard.email_update"))
+    else:
+        user = User.query.filter_by(
+            idx=session['user']['idx']
+        ).first()
+
+        session['user'] = {
+            "idx": user.idx,
+            "email": new_email['email'],
+            "nickname": user.nickname,
+        }
+
+        user.email = new_email['email']
+        db.session.commit()
 
     return redirect(url_for("dashboard.dashboard"))
