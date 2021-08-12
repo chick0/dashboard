c03e8975d862b98fd903c9546e161cec5cda3646
chick_0 <chick_0@ch1ck.xyz>
2021-08-12 23:08:38 +0900

    스코프 필터 추가

---
+++ app/scope.py
@@ -0,0 +1,14 @@
+v1 = {
+    "id": {
+        "display": "유저 아이디 읽기",
+        "access": "user.idx"
+    },
+    "email": {
+        "display": "이메일 주소 읽기",
+        "access": "user.email"
+    },
+    "nickname": {
+        "display": "닉네임 읽기",
+        "access": "user.nickname"
+    },
+}
--- app/template_filter.py
+++ app/template_filter.py
@@ -1,2 +1,12 @@
+def scope_display(s: str):
+    from .scope import v1
+
+    result = []
+    for this in sorted(list(set(s.split("-")))):
+        if this in v1.keys():
+            result.append(v1[this]['display'])
+
+    return ", ".join(result)
+
 
 filter_list = [name for name in dir() if not name.startswith("_")]
