baeece145e1bf4302820cbb1db606f461d917658
chick_0 <chick_0@ch1ck.xyz>
2021-08-13 14:02:40 +0900

    OAuth 코드 생성 기능

--- app/views/oauth.py
+++ app/views/oauth.py
@@ -1,5 +1,9 @@
+from uuid import uuid4
+from time import time
+from secrets import token_bytes
 
 from flask import Blueprint
+from flask import abort
 from flask import request
 from flask import session
 from flask import redirect
@@ -7,8 +11,11 @@ from flask import url_for
 from flask import render_template
 
 from app import db
+from app.models import Application
+from app.models import Code
 from app.models import Token
 from app.check import is_login
+from app.custom_error import OAuthTimeOut
 
 
 bp = Blueprint(
@@ -23,27 +30,66 @@ def ask():
     if not is_login():
         return redirect(url_for("dashboard.login.form"))
 
-    app_id = request.args.get("app_id", None)
-    scope = request.args.get("scope", None)
+    try:
+        app_id = int(request.args.get("app_id", None))
+        if app_id <= 0:
+            return abort(400)
+    except ValueError:
+        return abort(400)
 
-    # TODO:유저가 로그인한 상태가 아니라면 dashboard.login 으로 이동
-    # TODO:앱이 요구하는 스코프(권한) 유저한테 허용하냐고 물어봄
+    app = Application.query.filter_by(
+        idx=app_id
+    ).first()
+    if app is None:
+        return abort(404)
+
+    scope = request.args.get("scope", "id")
+
+    key = uuid4().__str__()
+    session[f"oauth:{key}"] = {
+        "app_id": app.idx,
+        "scope": scope,
+        "user_id": session['user']['idx'],
+        "time": int(time())
+    }
 
     return render_template(
-        "oauth/ask.html"
+        "oauth/ask.html",
+        app=app,
+        scope=scope,
+        key=key
     )
 
 
-@bp.post("")
-def callback():
-    app_id = request.form.get("app_id", None)
-    scope = request.form.get("scope", None)
+@bp.get("/<string:key>")
+def callback(key: str):
+    if not is_login():
+        return redirect(url_for("dashboard.login.form"))
 
-    # TODO:토큰 생성 코드 생성하기
-    # TODO:등록된 앱의 콜백 URL으로 리다이렉트
+    oauth_data = session.get(f"oauth:{key}", None)
+    if oauth_data is None:
+        return abort(400)
 
-    callback_url = "/"
-    return redirect(callback_url)
+    if int(time()) - oauth_data['time'] >= 300:
+        raise OAuthTimeOut
+
+    app = Application.query.filter_by(
+        idx=oauth_data['app_id']
+    ).first()
+    if app is None:
+        return abort(404)
+
+    code = Code()
+    code.application_idx = app.idx
+    code.target_idx = oauth_data['user_id']
+    code.scope = oauth_data['scope']
+    code.code = token_bytes(16).hex()
+
+    db.session.add(code)
+    db.session.commit()
+
+    del session[f"oauth:{key}"]
+    return redirect(app.callback + f"?code={code.code}")
 
 
 @bp.get("/revoke/<string:app_idx>")
