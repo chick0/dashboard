36eb94d92ed459a4f0a41c8f02ed1bdbd489d12f
chick_0 <chick_0@ch1ck.xyz>
2021-08-13 11:16:34 +0900

    텍스트 수정

--- app/scope.py
+++ app/scope.py
@@ -1,14 +1,14 @@
 v1 = {
     "id": {
-        "display": "유저 아이디 읽기",
+        "display": "유저 아이디",
         "access": "user.idx"
     },
     "email": {
-        "display": "이메일 주소 읽기",
+        "display": "이메일 주소",
         "access": "user.email"
     },
     "nickname": {
-        "display": "닉네임 읽기",
+        "display": "닉네임",
         "access": "user.nickname"
     },
 }
