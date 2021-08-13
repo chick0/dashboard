2eddbb620e6b9611b64b7920e4635b2b32af51f1
chick_0 <chick_0@ch1ck.xyz>
2021-08-13 14:37:18 +0900

    API v1 TODO

--- app/api/v1.py
+++ app/api/v1.py
@@ -2,6 +2,14 @@
 from flask import Blueprint
 from flask import jsonify
 
+from app import db
+from app.models import User
+from app.models import Application
+from app.models import ApplicationSecret
+from app.models import Code
+from app.models import Token
+from app.scope import v1
+
 
 bp = Blueprint(
     name="v1",
@@ -9,12 +17,31 @@ bp = Blueprint(
     url_prefix="/v1"
 )
 
-# TODO:유저 토큰 권한에 알맞는 유저 정보 리턴하기
-# TODO:OTP 코드 받아서 검증하는 API
 
+@bp.get("/token")
+def token():
+    # TODO:토큰 생성 코드를 이용해 토큰 생성하고 리턴하기
+    # TODO:코드 만료시 400 코드 만료됨 리턴하기
 
-@bp.get("")
-def hello():
+    # TODO:코드 사용후 삭제하기
+
     return jsonify({
-        "message": "Hello, World!"
+        "status": "todo"
+    })
+
+
+@bp.get("/user")
+def user():
+    # TODO:토큰 받고 만료되었는지 확인하기
+    # TODO:만료된 토큰 사용시 400 토큰 만료됨 리턴하기
+
+    # TODO:스코프에 알맞는 정보만 리턴하기
+
+    return jsonify({
+        "status": "todo",
+        "user": {
+            "id": int,
+            "email": str,
+            "nickname": str,
+        }
     })
