119a4f1d570420356811d12416e5f99bd9f7399a
chick_0 <chick_0@ch1ck.xyz>
2021-08-15 00:45:17 +0900

    주석 작업

--- app/ip.py
+++ app/ip.py
@@ -4,11 +4,15 @@ from flask import request
 
 def get_ip() -> str:
     for header in [
-        "CF-Connecting-IP",
-        "X-Forwarded-For",
+        "CF-Connecting-IP",  # 클라우드플레어에서 전달받은 IP 주소 값
+        "X-Forwarded-For",   # 리버스 프록시 서버에서 전달받은 IP 주소 값
     ]:
         tmp = request.headers.get(header, "")
+
+        # IP 주소가 7자 보다 길고 127.0.0.1 이 아니라면
         if len(tmp) >= 7 and tmp != "127.0.0.1":
+            # 비표준 헤더에서 가져온 IP 주소 값 리턴하기
             return tmp
 
+    # IP 정보를 저장하고 있는 비표준 헤더가 없다면 표준 헤더에서 가져오기
     return request.remote_addr
