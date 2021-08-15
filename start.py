83c5a3ed6828315ed55dfa20c7fe47e6584ea3c2
chick_0 <chick_0@ch1ck.xyz>
2021-08-15 20:43:39 +0900

    파일 삭제

--- start.py
+++
@@ -1,9 +0,0 @@
-
-from waitress import serve
-from paste.translogger import TransLogger
-
-from app import create_app
-
-
-if __name__ == "__main__":
-    serve(TransLogger(create_app()), port=16484)
