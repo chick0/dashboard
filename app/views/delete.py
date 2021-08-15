aeebb2bac5707e04501bac5d726a1908540b80df
chick_0 <chick_0@ch1ck.xyz>
2021-08-16 02:22:49 +0900

    탈퇴 기능 추가

--- app/views/delete.py
+++ app/views/delete.py
@@ -1,5 +1,14 @@
 
 from flask import Blueprint
+from flask import session
+from flask import redirect
+from flask import url_for
+from flask import render_template
+
+from app import db
+from app.models import User
+from app.models import Application
+from app.check import is_login
 
 
 bp = Blueprint(
@@ -11,7 +20,50 @@ bp = Blueprint(
 
 @bp.get("/ask")
 def ask():
-    # TODO:회원탈퇴 물어보기
-    # TODO:생성한 어플이 있으면 탈퇴 취소
+    # 로그인 상태가 아니라면 로그인 화면으로 이동하기
+    if not is_login():
+        return redirect(url_for("dashboard.login.form"))
 
-    return "todo"
+    # 만약 탈퇴하려는 유저가 생성한 어플리케이션이 있다면 탈퇴 실패
+    if Application.query.filter_by(
+        owner_idx=session['user']['idx'],
+        delete=False
+    ).first() is not None:
+        return render_template(
+            "dashboard/delete/failed.html"
+        )
+
+    return render_template(
+        "dashboard/delete/ask.html"
+    )
+
+
+@bp.post("/ask")
+def ask_post():
+    # 로그인 상태가 아니라면 로그인 화면으로 이동하기
+    if not is_login():
+        return redirect(url_for("dashboard.login.form"))
+
+    # 만약 탈퇴하려는 유저가 생성한 어플리케이션이 있다면 탈퇴 실패
+    if Application.query.filter_by(
+        owner_idx=session['user']['idx'],
+        delete=False
+    ).first() is not None:
+        return render_template(
+            "dashboard/delete/failed.html"
+        )
+
+    # 로그인한 유저의 계정 삭제하기
+    User.query.filter_by(
+        idx=session['user']['idx']
+    ).delete()
+
+    # 변경사항 저장하기
+    db.session.commit()
+
+    # 세션에 저장된 모든 값 삭제하기
+    for key in list(session.keys()):
+        del session[key]
+
+    # 로그인 화면으로 이동하기
+    return redirect(url_for("dashboard.login.form"))
