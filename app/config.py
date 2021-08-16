7be95eec039eab42018ad0cd20e7d78cd1dcedca
chick_0 <chick_0@ch1ck.xyz>
2021-08-16 23:27:24 +0900

    시크릿 키 관련 수정사항

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 from os import environ
-from secrets import token_bytes
+
+from .secret_key import SECRET_KEY
 
 
 # Session
-SECRET_KEY = token_bytes(32)
 SESSION_COOKIE_NAME = "s"
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SAMESITE = "Strict"
